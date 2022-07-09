4607e69cbadbe83cbe729bc9edd6b1ab7f0a9aee
Josip Maretić <83918157+Josip221@users.noreply.github.com>
2022-07-09 21:08:33 +0200

    Final

--- main.cpp
+++ main.cpp
@@ -1,75 +1,58 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
-#include <Firebase_ESP_Client.h>
-#include "addons/TokenHelper.h"
-#include "addons/RTDBHelper.h"
-
-#include <time.h>
-#include <Wire.h>
-#include <SPI.h>
-#include <Adafruit_Sensor.h>
-//#include "Adafruit_BME680.h"
-#include <Adafruit_BME280.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClientSecure.h>
+#include <Adafruit_BMP280.h>
 
 #define SEALEVELPRESSURE_HPA (1013.25)
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
 
-//fix this later
-#define WIFI_SSID "WemosD1"
-#define WIFI_PASSWORD "123456789"
-#define API_KEY "AIzaSyD-d6NBZprdkXabaFzRbMXyyoLaDK263Kg"
-#define DATABASE_URL "https://wemos-firebase-demo-default-rtdb.europe-west1.firebasedatabase.app/" 
+WiFiClientSecure client;
+HTTPClient https;
 
-// #define BME_SCK 13
-// #define BME_MISO 12
-// #define BME_MOSI 11
-// #define BME_CS 10
+Adafruit_BMP280 bmp;
 
-//Adafruit_BME680 bme;
-Adafruit_BME280 bme;
+const char* host = "https://shrouded-badlands-04100.herokuapp.com";
+const char* fingerprint = "346a4f3549d78bc1ea74b99ec9a8c91f5e6c2761";
 
-//Adafruit_BME280 bme(BME_CS); // hardware SPI
-// Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO,  BME_SCK);
+int ledPin = D6;
 
+float temperature;
+int pressure;
+float altitude;
 
-FirebaseData fbdo;
-FirebaseAuth auth;
-FirebaseConfig config;
-FirebaseJson json;
-
-bool signupOK = false;
-bool measureStatus = 0;
-
-bool readMeasureStatus();
 void initWiFi();
-void sendData(String path, float value);
-
+void printMeasurements();
+void printGoodbye();
+void sendPOST();
 
 void setup(){
   Serial.begin(115200);
+  delay(10);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);
+  Serial.println("Waking up from deep sleep.");
   initWiFi();
-  //initBME();
 
-  bme.begin(0x76);
-}
-
-void loop(){
-  if(readMeasureStatus()){
-    time_t timeStamp = time(0);
-    json.add("temperature", bme.readTemperature());
-    json.add("humidity", bme.readHumidity());
-    json.add("pressure", bme.readPressure());
-    json.add("timeStamp", timeStamp);
-    Serial.print(timeStamp);
-    delay(50);
-    if(Firebase.ready() && signupOK){
-      Firebase.RTDB.pushJSON(&fbdo, "measure", &json);
-      Firebase.RTDB.setInt(&fbdo, "measureStatus", 0);
-    }
+  bool status = bmp.begin(0x76); 
+  if (!status) {
+    Serial.println("Could not find a valid bmp280 sensor, check wiring!");
   }
-  delay(1000);
+  temperature = bmp.readTemperature();
+  pressure = bmp.readPressure() / 100.0F;
+  altitude = bmp.readAltitude(SEALEVELPRESSURE_HPA);
+  printMeasurements();
+  if(WiFi.status() == WL_CONNECTED){
+    sendPOST();
+  }
+  digitalWrite(ledPin, LOW);
+  printGoodbye();
+  ESP.deepSleep(600e6);
 }
-
-
+ 
+void loop(){
+}
 
 void initWiFi(){
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
@@ -82,38 +65,41 @@ void initWiFi(){
   Serial.print("Connected with IP: ");
   Serial.println(WiFi.localIP());
   Serial.println();
-
-  config.api_key = API_KEY;
-  config.database_url = DATABASE_URL;
-
-  if (Firebase.signUp(&config, &auth, "", "")){
-    Serial.println("ok");
-    signupOK = true;
-  }
-  else{
-    Serial.printf("%s\n", config.signer.signupError.message.c_str());
-  }
-
-  config.token_status_callback = tokenStatusCallback;
-  
-  Firebase.begin(&config, &auth);
-  Firebase.reconnectWiFi(true);
 }
 
-bool readMeasureStatus() {
-  Firebase.RTDB.getInt(&fbdo, "measureStatus");
-  if(fbdo.to<int>() == 1){
-    return 1;
-  }
-  return 0;
-}
-  
-// int initBME(){
-//   bme.setTemperatureOversampling(BME680_OS_8X);
-//   bme.setHumidityOversampling(BME680_OS_2X);
-//   bme.setPressureOversampling(BME680_OS_4X);
-//   // bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
-//   // bme.setGasHeater(320, 150); // 320*C for 150 ms
-//   return 0;
-// }
+void printMeasurements(){
+  Serial.print("Temperature = ");
+    Serial.print(temperature);
+    Serial.println(" *C");
 
+    Serial.print("Pressure = ");
+    Serial.print(pressure);
+    Serial.println(" hPa");
+
+    Serial.print("Approx. Altitude = ");
+    Serial.print(altitude);
+    Serial.println(" m");
+}
+
+void sendPOST(){
+  client.setFingerprint(fingerprint);
+    if(https.begin(client, host)){  
+      https.addHeader("Content-Type", "application/json");
+      int httpsCode = https.POST("{\"temperature\":\"" + String(temperature) + "\",\"altitude\":\"" + String(altitude) + "\",\"pressure\":\"" + pressure + "\"}");
+      if(httpsCode > 0){
+        String payload = https.getString();
+        Serial.println(payload);
+      }
+
+      https.end();
+    }else{
+      Serial.println("Unable to connect");
+    };
+}
+
+void printGoodbye(){
+    Serial.print("Device was active for: ");
+    Serial.print(millis() / 1000);
+    Serial.println(" seconds.");
+    Serial.println("Going to deep sleep.");
+}
